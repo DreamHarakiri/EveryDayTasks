4ae69edf0a02b57457e9051247712fa0682fd6ed
MrTheSumraak <maew202139@gmail.com>
2025-01-28 17:16:28 +0300

    register

--- src/components/register/register.tsx
+++ src/components/register/register.tsx
@@ -7,6 +7,8 @@ import { getRegisterData } from '../../service/Asyncs/register';
 import { getRegisterLoading } from '../../service/slices/user.slice';
 import { Preloader } from '../ui/preloader/preloader';
 
+const logo = require('../register/images/user.jpg');
+
 export const RegisterPage = () => {
   const [email, setEmail] = useState<string>('');
   const [name, setName] = useState<string>('');
@@ -19,52 +21,45 @@ export const RegisterPage = () => {
     dispatch(getRegisterData({ name, email, password }));
   };
   return (
-    <div className={styles.body}>
-      <div className={styles.registerPanel}>
-        {isLoading ? (
-          <Preloader />
-        ) : (
-          <>
-            <div className={styles.headerStyle}>
-              <h1>
-                EveryDay Tasks
-                <span>
-                  <BracesAsterisk className={styles.logoIcon} size={35} />
-                </span>
-              </h1>
-              <p>Для входа в аккаунт введите данные в поля ниже</p>{' '}
-            </div>
-            <input
-              type='text'
-              placeholder='Введите имя'
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              className={styles.input}
-            />
-            <input
-              type='email'
-              placeholder='Введите email'
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className={styles.input}
-            />
-            <input
-              type='password'
-              placeholder='Введите пароль'
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className={styles.input}
-            />
-            <button
-              onClick={handleRegister}
-              type='submit'
-              className={styles.button}
-            >
-              Зарегистрироваться
+    <>
+      <div className={styles.page}>
+        <div className={styles.registerPanell}>
+          <div className={styles.authorization_wrapper}>
+            <section className={styles.authorization}>
+              <h2 className={styles.headingAuthorization}>Есть аккаунт?</h2>
+              <p>Достаточно просто авторизоваться!</p>
+              <button type='submit'>Авторизация</button>
+            </section>
+          </div>
+          <section className={styles.formRegister}>
+            <h2>Ты тут впервые?</h2>
+            <p>Давай знакомиться! Заполни поля ниже</p>
+            <form action=''>
+              <fieldset className={styles.formRegister__borderNone}>
+                {/* <label className={styles.Username} htmlFor='text'>
+                  Username
+                </label> */}
+                <img
+                  className={styles.userNameIcon}
+                  alt='logo'
+                  src={String(logo)}
+                />
+                <input
+                  className={styles.Username_input}
+                  type='text'
+                  placeholder='Username'
+                  id='text'
+                />
+                <input type='email' placeholder='Email Address' />
+                <input type='password' placeholder='Password' />
+              </fieldset>
+            </form>
+            <button type='submit' className={styles.formRegister__button}>
+              Продолжить
             </button>
-          </>
-        )}{' '}
-      </div>{' '}
-    </div>
+          </section>
+        </div>
+      </div>
+    </>
   );
 };
